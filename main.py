fec3e86ceeabcdf8a52ad65f60614be04a97f4b9
user <tixan84@gmail.com>
2026-02-03 13:26:17 +0200

    create a paddle

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from turtle import Screen
+from paddle import Paddle
 
 screen = Screen()
 screen.screensize(800, 600, "black")
 screen.title("Pong")
 
+user_paddle = Paddle()
+user_paddle.goto(350, 0)
+
+screen.listen()
+
+screen.onkey(user_paddle.go_up, "Up")
+screen.onkey(user_paddle.go_down, "Down")
 
 screen.exitonclick()

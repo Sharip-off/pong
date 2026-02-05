a10a815fa0330bddf93505e9f9e5bf5e16f2a9ae
user <tixan84@gmail.com>
2026-02-05 12:33:47 +0200

    The ball bounces from top and bottom border of screen

--- main.py
+++ main.py
@@ -26,4 +26,10 @@ while game_is_on:
     screen.update()
     ball.move()
 
+    #Detect collision with wall
+    if ball.ycor() > 300 or ball.ycor() < -300:
+        ball.bounce()
+    
+
+
 screen.exitonclick()

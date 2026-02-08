f9624d4d0061fb30960b747f9a728e9d527ddea3
Valerii Sharipov <tixan84@gmail.com>
2026-02-08 23:03:28 +0200

    the pong is dine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 screen = Screen()
@@ -13,6 +14,7 @@ screen.tracer(0)
 r_paddle = Paddle((350, 0))
 l_paddle = Paddle((-350, 0))
 ball = Ball()
+score = Scoreboard()
 
 
 screen.listen()
@@ -25,7 +27,7 @@ screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -36,11 +38,13 @@ while game_is_on:
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
     #The ball misses the r_paddle
-    if ball.xcor() > 380 or ball.xcor() < -380:
+    if ball.xcor() > 380:
         ball.reset_position()
+        score.l_point()
     #The ball misses the l_paddle
     if ball.xcor() < -380:
         ball.reset_position()
+        score.r_point()
 
 
 screen.exitonclick()
